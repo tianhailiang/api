487af6a500c87e161db09e1c969102a62d74c4a7
wangshuqin <shuqin.wang@daxuezhang.com>
2018-03-05 11:07:16 +0800

    view_count

--- model/cms.js
+++ model/cms.js
@@ -1578,7 +1578,7 @@ exports.detail_count = function (data, callback) {
             redisPool.incr(data.uuid+"_"+data.catid+"_"+data.id+"_viewnum", function(){});
             update_viewnum(data.catid, data.id, data.uuid, callback);
             }else{
-              var viewNumKey = "WEB:HITS:cat_"+data.catid+"_"+data.id;
+              var viewNumKey = "WEB:HITS:cat_"+data.id;
               redisPool.get(viewNumKey, function(err, reply){
                 if(reply){
                   callback(null, {"uuid":data.uuid, "num":reply});
